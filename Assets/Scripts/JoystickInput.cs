e27dbb4166c73d59f5d9937e2fdab41df403c89f
Eugene <eugenemindset@gmail.com>
2020-01-08 16:30:21 -0500

    Created basic controls
    UI joystick now has player inputs. GameController takes those inputs and applies them to a character controller.

---
+++ Assets/Scripts/JoystickInput.cs
@@ -0,0 +1,55 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.EventSystems;
+
+
+public class JoystickInput : MonoBehaviour, IBeginDragHandler, IEndDragHandler, IDragHandler {
+
+	public Image stickPortion;
+	public Image gatePortion;
+	public Color gizmoColor;
+	public float maxMagnitude;
+
+	private Vector2 inputDir;
+	private float inputMag;
+
+	public Vector2 InputDirection { get => this.inputDir; }
+	public float InputMagnitude { get => this.inputMag; }
+	public void OnBeginDrag( PointerEventData eventData ) {
+		this.OnDrag( eventData );
+	}
+
+	public void OnEndDrag( PointerEventData eventData ) {
+		this.stickPortion.transform.position = this.gatePortion.transform.position;
+		this.inputDir = Vector2.zero;
+		this.inputMag = 0;
+	}
+
+	public void OnDrag( PointerEventData eventData ) {
+		Vector2 basePosition = new Vector2( this.gatePortion.rectTransform.position.x, this.gatePortion.rectTransform.position.y );
+		Vector2 dragVector = eventData.position - basePosition;
+
+		float magnitude = dragVector.magnitude;
+		Vector2 direction = dragVector.normalized;
+
+		magnitude = magnitude > this.maxMagnitude ? this.maxMagnitude : magnitude;
+
+		this.stickPortion.rectTransform.anchoredPosition = direction * magnitude;
+
+		this.inputDir = direction;
+		this.inputMag = magnitude / this.maxMagnitude;
+	}
+
+
+	private void OnDrawGizmos( ) {
+		float zOff = Camera.main.nearClipPlane;
+		Vector3 stickPos = new Vector3( this.stickPortion.rectTransform.position.x, this.stickPortion.rectTransform.position.y, zOff );
+		Vector3 gatePos = new Vector3( this.gatePortion.rectTransform.position.x, this.gatePortion.rectTransform.position.y, zOff );
+
+		Gizmos.color = this.gizmoColor;
+		Gizmos.DrawLine( Camera.main.ScreenToWorldPoint( stickPos ), Camera.main.ScreenToWorldPoint( gatePos ) );
+	}
+}
+
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour {
+
+	public JoystickInput moveInputs;
+	public JoystickInput rotateInputs;
+	public CharacterController charController;
+
+	public float moveSpeed;
+
+	void Start( ) {
+
+	}
+
+	// Update is called once per frame
+	void FixedUpdate( ) {
+		Vector3 moveDir = new Vector3( this.moveInputs.InputDirection.x, 0, this.moveInputs.InputDirection.y );
+		Vector3 lookDir = new Vector3( this.rotateInputs.InputDirection.x, 0, this.rotateInputs.InputDirection.y );
+
+		moveDir *= this.moveSpeed * this.moveInputs.InputMagnitude;
+
+		this.charController.SimpleMove( moveDir );
+		this.charController.transform.LookAt( this.charController.transform.position + lookDir );
+	}
+}
